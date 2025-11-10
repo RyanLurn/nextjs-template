0966bf2cc620cc64bf2277aeab04e541268156e6
Ryan Luong <lumintriet@gmail.com>
2025-11-10 10:06:29 +0700

    fix: add type marker to import in theme provider

--- src/components/providers/theme.tsx
+++ src/components/providers/theme.tsx
@@ -1,7 +1,8 @@
 "use client";
 
+import type { ComponentProps } from "react";
+
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { ComponentProps } from "react";
 
 function ThemeProvider({
   children,
