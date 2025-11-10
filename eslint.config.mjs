b2367c0a80ac868bf04e67791e508973a5ee8434
Ryan Luong <lumintriet@gmail.com>
2025-11-10 08:15:39 +0700

    chore: add typescript-eslint and configure type-aware linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,17 @@
 
 import { defineConfig, globalIgnores } from "eslint/config";
 import js from "@eslint/js";
+import ts from "typescript-eslint";
 
 export default defineConfig([
   globalIgnores([".next/", "out/", "build/", "next-env.d.ts"]),
   js.configs.recommended,
+  ts.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  },
 ]);
